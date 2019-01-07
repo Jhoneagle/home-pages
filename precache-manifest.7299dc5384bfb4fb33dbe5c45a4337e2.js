dcb5cca408ab73fd8e8d1981f0e54e9d362232a3
Jhoneagle <eaglekjohn@gmail.com>
2019-01-07 19:34:57 +0200

    Updates

---
+++ precache-manifest.7299dc5384bfb4fb33dbe5c45a4337e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cbbb014db88debd7bcf",
+    "url": "/home-pages/static/js/runtime~main.9cbbb014.js"
+  },
+  {
+    "revision": "8efd92393ba2abc74345",
+    "url": "/home-pages/static/js/main.8efd9239.chunk.js"
+  },
+  {
+    "revision": "c46d1f0c95e8e3063009",
+    "url": "/home-pages/static/js/1.c46d1f0c.chunk.js"
+  },
+  {
+    "revision": "c9f8684ed2609028ddcc56888a9cd7ff",
+    "url": "/home-pages/index.html"
+  }
+];
