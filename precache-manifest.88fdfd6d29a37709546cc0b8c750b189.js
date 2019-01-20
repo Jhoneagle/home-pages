6abed7e8ccf09775c462acfef56e52bf1aab897e
Jhoneagle <eaglekjohn@gmail.com>
2019-01-20 15:04:56 +0200

    Updates

---
+++ precache-manifest.88fdfd6d29a37709546cc0b8c750b189.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cbbb014db88debd7bcf",
+    "url": "/home-pages/static/js/runtime~main.9cbbb014.js"
+  },
+  {
+    "revision": "e90551acb4155aafea65",
+    "url": "/home-pages/static/js/main.e90551ac.chunk.js"
+  },
+  {
+    "revision": "1a197cf1b63b3569c102",
+    "url": "/home-pages/static/js/1.1a197cf1.chunk.js"
+  },
+  {
+    "revision": "d8e7c3eb879d0cee386bf9d47a20cefb",
+    "url": "/home-pages/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/home-pages/precache-manifest.d5e9037405854788250580943774b9be.js"
+  "/home-pages/precache-manifest.88fdfd6d29a37709546cc0b8c750b189.js"
 );
 
 workbox.clientsClaim();
