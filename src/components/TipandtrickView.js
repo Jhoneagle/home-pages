c205c25bc0b55aa443f0311adafef2cafaefa713
Jhoneagle <eaglekjohn@gmail.com>
2020-09-17 12:33:54 +0300

    update views

--- src/components/TipandtrickView.js
+++ src/components/TipandtrickView.js
@@ -60,19 +60,19 @@ class TipandtrickView extends React.Component {
             </h2>
         
             <p>
-              On this one unfortunately if you don´t speak finnish then this might not really help you at all. But if your smart you can always translate the content with google so it´s not impossible but it´s not the most easiest to use.
+              { text.accessibility1 }
             </p>
           
             <p>
-              So yeah if your example content creator or web designer or something like that you might actually like this a lot. There is website called <a href="http://www.saavutettavuus.fi/kontrasti.php">saavutettavuus.fi</a> which contains a lot of simple stuff about accessibility and colors.
+              So yeah if your example content creator or web designer or something like that you might actually like this a lot. There is website called <a href="http://www.saavutettavuus.fi/kontrasti.php">saavutettavuus.fi</a> which contains a lot of simple stuff about accessibility and colours.
             </p>
           
             <p>
-              For example do you need to know contrast diffirent between colors or how well you can see text from background or maybe you want to find a new color. These all can be found from the website. The website also contains lot of other stuff about accessibility too but the color part s one of the most important in the end.
+              { text.accessibility3 }
             </p>
           
             <p>
-              Also as an extra note, not to brag or anything, I actually designed most of the logic behind the websites color functionality according to specifics that was given to me. Like how to chance RGB to hexadecimal and vice versa, how to go through all the colors in RGB color table and how to compare colors between each others. But I never actually implemented it there on the PHP.
+              { text.accessibility4 }
             </p>
           </div>
         </div>
